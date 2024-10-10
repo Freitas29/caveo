0479f6ec0c26d0f0f0098d0fc546dbffffe0a8a0
Freitas29 <arao.freitas@hotmail.com>
2024-10-09 21:54:59 -0300

    feat: Listagem dos cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,76 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import { container } from "../../inversify.config";
+import { queryOptions, useSuspenseQuery } from "@tanstack/react-query";
+import { IListProductsGateway, listProductsGatewayDI, ProductResponse } from "@/domain/gateways/products/ListProductsGateway";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import {  CardActionArea, CardMedia, Grid2, Typography } from "@mui/material";
+
+
+function RenderCard(props: ProductResponse) {
+  return (
+    <Card sx={{ maxWidth: "100%", minHeight: "20rem",  maxHeight: "20rem" }}>
+      <CardActionArea sx={{
+          maxHeight: "20rem",
+          minHeight: "20rem",
+        }}>
+        <CardMedia
+          component="img"
+          sx={{
+            objectFit: "contain",
+            maxHeight: "8rem",
+          }}
+          image={props.image}
+          alt={props.title}
+        />
+        <CardContent>
+          <Typography 
+          sx={{ 
+            color: 'text.secondary', 
+            overflow: 'hidden',
+            textOverflow: 'ellipsis',
+            display: '-webkit-box',
+            WebkitLineClamp: '2',
+            WebkitBoxOrient: 'vertical', 
+          }} 
+          variant="h5" 
+          component="div">
+            {props.title}
+          </Typography>
+          <Typography variant="body2" 
+            sx={{
+            color: 'text.secondary',
+            overflow: 'hidden',
+            textOverflow: 'ellipsis',
+            display: '-webkit-box',
+            WebkitLineClamp: '3',
+            WebkitBoxOrient: 'vertical', }}>
+            {props.description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const gateway = container.get<IListProductsGateway>(listProductsGatewayDI)
+  const productsOptions = queryOptions({
+      queryKey: ['products'],
+      queryFn: async () => {
+          const response = gateway.getProducts()
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+          return response
+      },
+  })
+
+  const products = useSuspenseQuery(productsOptions)
+
+  return (
+    <Grid2 container spacing={2}>
+      {products.data.map(item => (
+        <Grid2 size={4} key={item.id}> {RenderCard(item)} </Grid2>
+      ))}
+    </Grid2>
+  )
 }
